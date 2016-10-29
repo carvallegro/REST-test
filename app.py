6795c6b4afb8782d95424f1f1cc4a57892d46ef8
carvallegro <valentin.carmignac@gmail.com>
2016-10-29 16:42:56 +0200

    deleted useless files

--- app.py
+++ app.py
@@ -8,23 +8,9 @@ from src.services import wikipedia
 
 app = Flask(__name__)
 
-
-def binary_to_dict(the_binary):
-    jsn = ''.join(chr(int(x, 2)) for x in the_binary.split())
-    d = json.loads(jsn)
-    return d
-
 @app.route('/')
 def hello_world():
     return 'Hello World!'
-
-@app.route('/wiki/<query>')
-def wiki(query):
-    try:
-        return wikipedia(query)
-    except HTTPError as e:
-        return "Error during completion"
-
 
 if __name__ == '__main__':
     configuration.load("configuration.dev.yaml")
