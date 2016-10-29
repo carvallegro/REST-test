4e81110e6b8d1e7d500795b3884e12532c6b447a
carvallegro <valentin.carmignac@gmail.com>
2016-10-29 17:05:52 +0200

    corrected bad imports

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 import json
 
 from flask import Flask
-from requests import HTTPError
-
-from src import configuration
-from src.services import wikipedia
 
 app = Flask(__name__)
 
@@ -13,7 +9,6 @@ def hello_world():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    configuration.load("configuration.dev.yaml")
     app.run()
 
 
