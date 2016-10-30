f55b7789307dc37fe980c6511b996db93978f2a5
carvallegro <valentin.carmignac@gmail.com>
2016-10-30 16:34:35 +0100

    updated rocket test

--- app.py
+++ app.py
@@ -11,7 +11,16 @@ def hello_world():
 @app.route('/test')
 def test_rocketchat():
     return json.dumps({
-           "text":"HOLa fdfs"
+           "text":"HOLa fdfs",
+            "attachments": [
+                {
+                    "title": "Rocket.Chat",
+                    "title_link": "https://rocket.chat",
+                    "text": "Rocket.Chat, the best open source chat",
+                    "image_url": "https://rocket.chat/images/mockup.png",
+                    "color": "#764FA5"
+                }
+            ]
            })
 
 if __name__ == '__main__':
